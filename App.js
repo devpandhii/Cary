ffd7339a965794641c00717a9d70b87b41f698bb
Dev <devpandhi1@gmail.com>
2024-03-29 20:14:14 +0530

    login signup page created

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import Login from './Screens/Login';
+import Signup from './Screens/Signup';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello Dev!!!!</Text>
+      <Signup />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +14,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: StatusBar.currentHeight,
   },
 });
